7fd3c4928e19975921491f34c3a5c1c5817b1e71
axelbrians-pegipegi <axel.briano@pegipegi.com>
2022-03-24 18:23:51 +0700

    feat: add thumbnail placeholder when not playing

--- app/src/main/java/com/axelb/rails/presentation/EndlessVideoVerticalPager.kt
+++ app/src/main/java/com/axelb/rails/presentation/EndlessVideoVerticalPager.kt
@@ -1,27 +1,21 @@
 package com.axelb.rails.presentation
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.material.Surface
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.LinearGradientShader
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.viewinterop.AndroidView
 import androidx.compose.ui.zIndex
 import com.axelb.rails.R
 import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.PagerState
 import com.google.accompanist.pager.VerticalPager
 import com.google.accompanist.pager.rememberPagerState
 import com.google.android.exoplayer2.ExoPlayer
@@ -44,37 +38,39 @@ fun EndlessVideoVerticalPager(
         R.raw.gidle_tomboy,
         R.raw.video_kucing_masak
     )
+    val addedThumbnailId = listOf(
+        R.drawable.gidle_tomboy,
+        R.drawable.video_kucing
+    )
     val videoItems = remember {
         mutableStateListOf(
             R.raw.gidle_tomboy,
             R.raw.video_kucing_masak
         )
     }
-
-    val context = LocalContext.current
-    val mediaItem = remember {
-        val resourceId = R.raw.gidle_tomboy
-        val videoUri = RawResourceDataSource.buildRawResourceUri(resourceId)
-        MediaItem.Builder()
-            .setUri(videoUri)
-            .setMediaId(resourceId.toString())
-            .setTag(videoUri)
-            .setMediaMetadata(
-                MediaMetadata.Builder()
-                    .setDisplayTitle("GIDLE TOMBOY Challenge")
-                    .build()
-            )
-            .build()
+    val thumbnailItems = remember {
+        mutableStateListOf(
+            R.drawable.gidle_tomboy,
+            R.drawable.video_kucing
+        )
     }
+    val isAutoPlay = true
+    val context = LocalContext.current
     val exoPlayer = remember {
         ExoPlayer.Builder(context).build().apply {
-            this.setMediaItem(mediaItem)
             this.prepare()
             this.playWhenReady = true
             this.repeatMode = Player.REPEAT_MODE_ONE
         }
     }
 
+    val currentVideo = if (isAutoPlay) {
+        determineCurrentlyPlayingItem(pagerState, videoItems)
+    } else {
+        null
+    }
+
+    updateCurrentlyPlayingItem(exoPlayer = exoPlayer, video = currentVideo)
     DisposableEffect(
         VerticalPager(
             count = videoItems.size,
@@ -84,6 +80,7 @@ fun EndlessVideoVerticalPager(
 
             if (currentPage == videoItems.lastIndex) {
                 videoItems.addAll(addedItems)
+                thumbnailItems.addAll(addedThumbnailId)
                 Timber.d("itemCount increased to ${itemCount.value}")
             }
 
@@ -97,8 +94,12 @@ fun EndlessVideoVerticalPager(
 //                contentScale = ContentScale.Crop
 //            )
                 VideoPlayerComposable(
-                    modifier = Modifier.fillMaxSize(),
-                    exoPlayer = exoPlayer
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .zIndex(1f),
+                    exoPlayer = exoPlayer,
+                    thumbnailId = thumbnailItems[pageIndex],
+                    isCurrentShownPage = pageIndex == currentPage
                 )
                 OverlayVideoVerticalPager()
                 Box(
@@ -126,16 +127,49 @@ fun EndlessVideoVerticalPager(
     }
 }
 
-private fun determineCurrentlyPlayingItem(listState: LazyListState, items: TweetItems): TweetItem? {
-    val layoutInfo = listState.layoutInfo
-    val visibleTweets = layoutInfo.visibleItemsInfo.map { items[it.index] }
-    val tweetsWithVideo = visibleTweets.filter { it.hasAnimatedMedia }
-    return if (tweetsWithVideo.size == 1) {
-        tweetsWithVideo.first()
-    } else {
-        val midPoint = (layoutInfo.viewportStartOffset + layoutInfo.viewportEndOffset) / 2
-        val itemsFromCenter =
-            layoutInfo.visibleItemsInfo.sortedBy { abs((it.offset + it.size / 2) - midPoint) }
-        itemsFromCenter.map { items[it.index] }.firstOrNull { it.hasAnimatedMedia }
+@Composable
+private fun updateCurrentlyPlayingItem(exoPlayer: ExoPlayer, video: Int?) {
+    LaunchedEffect(video) {
+        exoPlayer.apply {
+            if (video != null) {
+                val videoUri = RawResourceDataSource.buildRawResourceUri(video)
+                val mediaItem = MediaItem.Builder()
+                        .setUri(videoUri)
+                        .setMediaId(video.toString())
+                        .setTag(videoUri)
+                        .setMediaMetadata(
+                            MediaMetadata.Builder()
+                                .setDisplayTitle("GIDLE TOMBOY Challenge")
+                                .build()
+                        )
+                        .build()
+
+                setMediaItem(mediaItem)
+                prepare()
+                playWhenReady = true
+            } else {
+                stop()
+            }
+        }
     }
+}
+
+@OptIn(ExperimentalPagerApi::class)
+private fun determineCurrentlyPlayingItem(pagerState: PagerState, videoItems: List<Int>): Int? {
+
+    val currentPage = pagerState.currentPage
+    return videoItems[currentPage]
+
+//
+//    val layoutInfo = pagerState.layoutInfo
+//    val visibleTweets = layoutInfo.visibleItemsInfo.map { videoItems[it.index] }
+//    val tweetsWithVideo = visibleTweets.filter { it.hasAnimatedMedia }
+//    return if (tweetsWithVideo.size == 1) {
+//        tweetsWithVideo.first()
+//    } else {
+//        val midPoint = (layoutInfo.viewportStartOffset + layoutInfo.viewportEndOffset) / 2
+//        val itemsFromCenter =
+//            layoutInfo.visibleItemsInfo.sortedBy { abs((it.offset + it.size / 2) - midPoint) }
+//        itemsFromCenter.map { videoItems[it.index] }.firstOrNull { it.hasAnimatedMedia }
+//    }
 }
